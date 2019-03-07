9a5d8ff94d426ea45e887a2303ca9b34ccf9f19d
Yahaira3 <46031280+Yahaira3@users.noreply.github.com>
2019-03-07 12:13:14 -0400

    echo
    echo

---
+++ practicas/primer_scrip_php/echo.php
@@ -0,0 +1,19 @@
+<?php 
+$variable1 ="yahaira";
+$variable2 = true;
+$variable3 = 22;
+
+echo $variable1 . "<br>";
+echo $variable2 . "<br>";
+echo $variable3 . "<br>";
+?>
+
+<?php 
+$variable1 =false;
+$variable2 = 22;
+$variable3 = "yahaira";
+
+echo $variable1 . "<br>";
+echo $variable2 . "<br>";
+echo $variable3 . "<br>";
+?>
