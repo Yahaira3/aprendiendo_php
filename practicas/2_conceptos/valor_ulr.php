6a2be389fd0f6624db616052a97f691e77aa3e26
Yahaira3 <yahairaj566@gmail.com>
2019-03-08 09:26:34 -0400

    uso de get
    get

---
+++ practicas/2_conceptos/valor_ulr.php
@@ -0,0 +1,22 @@
+<?php 
+$variable = "yahaira";
+echo $variable."<br><br>";
+var_dump($variable);
+
+$edad = 22;
+echo "<hr>";
+var_dump($edad);
+echo "<hr>";
+
+$poseecasa = true;
+
+echo "posee casa".$poseecasa;
+echo "<br>";
+var_dump($poseecasa);
+echo "<hr>";
+echo "el valor del pais es : ";
+echo $_GET["pais"];
+echo "<pre>";
+var_dump($_GET);
+echo "</pre>";
+?>
