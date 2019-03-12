c837cd6614dfd1178b4a322c22b663c6a6ca8dbc
yahaira <yahairaj566@gmail.com>
2019-03-12 12:20:17 -0400

    actualizar

--- public_html_proyecto/app/vistas/_head.php
+++ public_html_proyecto/app/vistas/_head.php
@@ -9,24 +9,7 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
 <!--incluir bootstrap-->
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-
-<style>
-#login  {
-display: flex;
-align-items: center;
-justify-content: center;
-height:100vh;
-}
-body a [Title]{
-    display:none;
-}
-#login form {
-    padding:30px;
-    border:solid 1px white;
-    border-radius:15px;
-     background:white;
-    color:black;
-}
-</style>
+<!--- incluir los estilos    --->
+<link rel = "stylesheet" href="recursos/css/style.css">
 
 </head>
--- public_html_proyecto/app/vistas/index.vista.php
+++ public_html_proyecto/app/vistas/index.vista.php
@@ -1,9 +1,16 @@
 <?php require "_head.php";?>
 <body>
 <div class="container">
+    <section  class= "d-flex vh-100 align-items-center justify-content-center flex-column">
+
 <h1><?= $saludo; ?></h1>
-<a href ="login.php">Iniciar Sesion</a>
-<a href ="registro.php">Resgistrarse</a>
+<br>
+<div>
+<a  class ="btn btn-primary" href ="login.php">Iniciar Sesion</a>
+<a class ="btn btn-primary" href ="registro.php">Resgistrarse</a>
+</div>
+    </section>
+
 </div>
     
 </body>
