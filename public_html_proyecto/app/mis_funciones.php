d9f321cdc8726da4ad8593330e8af36277c2f1cc
yahaira <yahairaj566@gmail.com>
2019-03-18 10:12:27 -0400

    ss

---
+++ public_html_proyecto/app/mis_funciones.php
@@ -0,0 +1,11 @@
+<?php
+function imprimir ($valor_recibido)
+{
+    echo "<pre>";
+    var_export($valor_recibido);
+    "</pre>";
+}
+function vistas ()
+{
+    
+}
--- public_html_proyecto/registro.php
+++ public_html_proyecto/registro.php
@@ -3,6 +3,7 @@ $titulo ="Registro";
 session_start();
 require "app/conexion.php";
 
+
 // Si el usuario está logeado no puede ver esta página
 if(isset($_SESSION['usuario_id']) == true) {
     // REDIRECCIONAR a la pagina principal
