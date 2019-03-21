d054af558d1695056f0052aec83a6cef4bcde769
yahaira <yahairaj566@gmail.com>
2019-03-21 11:41:32 -0400

    id

--- public_html_proyecto/nuevas_publicaciones.php
+++ public_html_proyecto/nuevas_publicaciones.php
@@ -10,7 +10,7 @@ if(isset($_POST['btnCrearPublicacion'])) {
         'titulo' => $_POST['inputTitulo'],
         'descripcion' => $_POST['inputDescripcion'],
         'id_categoria' => $_POST['selectCategoria'],
-        'nombre_archivo' => $_POST['inputArchivo'],
+        'nombre_archivo' => "",
         'creado_por' => $_SESSION['usuario_id']
     );
     // 1. Preparar el query
